1e9ee05e6179655ec26bf6c98b89389220c3e6db
Rajb2381991 <rajshekharbudharam2015@gmail.com>
2016-03-06 15:59:46 +0530

    Update EmailValidationTest.java

--- Assignment2/src/EmailValidationTest.java
+++ Assignment2/src/EmailValidationTest.java
@@ -3,14 +3,19 @@ import org.junit.Test;
 public class EmailValidationTest 
 {
 	@Test
+	public void testSuccess()
+	{
+		assertTrue("Valid",EmailValidation.isValidEmail("raj123@gmail.com"));
+		assertTrue("Valid",EmailValidation.isValidEmail("raj45raj@gmail.in"));
+		assertTrue("Valid",EmailValidation.isValidEmail("raj23@yahoo.in"));
+	}
+	@Test
 	public void testFailure() 
 	{
 		assertFalse("InValid",EmailValidation.isValidEmail("rajgmial.com"));
 		assertFalse("InValid",EmailValidation.isValidEmail("rajgmial."));
 		assertFalse("InValid",EmailValidation.isValidEmail("12344@gmail.com"));
 		assertFalse("InValid",EmailValidation.isValidEmail("1233raj"));
-		assertTrue("Valid",EmailValidation.isValidEmail("a1234@.com"));
-		assertTrue("Valid",EmailValidation.isValidEmail("raj123@gmail.com"));
-		assertTrue("Valid",EmailValidation.isValidEmail("raj45raj@gmail.in"));
+		assertFalse("InValid",EmailValidation.isValidEmail("a1234@.com"));
 	}
 }
