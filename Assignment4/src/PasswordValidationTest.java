401b134c8a80f4d84bab0005cb875b96f36991a8
Rajb2381991 <rajshekharbudharam2015@gmail.com>
2016-03-07 13:07:50 +0530

    Update PasswordValidationTest.java

--- Assignment4/src/PasswordValidationTest.java
+++ Assignment4/src/PasswordValidationTest.java
@@ -6,16 +6,16 @@ public class PasswordValidationTest
 	@Test
 	public void testFailure() 
 	{
-		assertFalse(PasswordValidation.isPasswordValid(""));
-		assertFalse(PasswordValidation.isPasswordValid("raj123456"));
+		assertFalse(PasswordValidation.isPasswordValid("raj1234"));
+		assertFalse(PasswordValidation.isPasswordValid("raj12356"));
 		assertFalse(PasswordValidation.isPasswordValid("156."));
 		assertFalse(PasswordValidation.isPasswordValid("...,/~`"));
 	}
 	@Test
 	public void testSuccess()
 	{
-		assertTrue(PasswordValidation.isPasswordValid("raj1234."));
-		assertTrue(PasswordValidation.isPasswordValid("1234567."));
-		assertTrue(PasswordValidation.isPasswordValid("ra5432~,"));
+		assertTrue(PasswordValidation.isPasswordValid("r@345608"));
+		assertTrue(PasswordValidation.isPasswordValid("@1234567"));
+		assertTrue(PasswordValidation.isPasswordValid("raj$rajH"));
 	}
 }
